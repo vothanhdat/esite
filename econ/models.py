03d8fccb4dc4bf3474b34ee836b7dab08bd51309
Vo Thanh Dat <vothanhdat@outlook.com>
2017-06-29 18:10:19 +0700

    missing

--- econ/models.py
+++ econ/models.py
@@ -39,6 +39,8 @@ class Promotion(models.Model) :
     promotion_name = models.CharField(max_length=100)
     promotion_type = models.CharField(max_length=1, choices=PROMOTION_TYPE,default='P')
     promotion_value = models.FloatField()
+    promotion_start = models.DateTimeField()
+    promotion_end = models.DateTimeField()
     # promotion_agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
     class Meta:
         abstract = True
