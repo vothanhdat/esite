00803e1ca5aad8a9ca917c83e049a6d4c5b195cb
Vo Thanh Dat <vothanhdat@outlook.com>
2017-07-07 16:07:07 +0700

    ss

--- econ/views/autocomplete.py
+++ econ/views/autocomplete.py
@@ -1,8 +1,36 @@
 from dal import autocomplete
 from django.conf.urls import url
+
 from ..models import SpecificDetail,Specific,Cagetory
 
 
+
+
+def loginRequired(original_class):
+    origin_get_queryset = original_class.get_queryset
+    origin_create_object = original_class.create_object
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated():
+            return []
+        else:
+            return origin_get_queryset(self)
+
+    def create_object(self,text):
+        if not self.request.user.is_authenticated():
+            return []
+        else:
+            return origin_create_object(self,text)
+
+
+    original_class.get_queryset = get_queryset 
+    original_class.create_object = create_object 
+
+    return original_class
+
+
+
+@loginRequired
 class SpecificDetailAutoComplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
 
@@ -10,6 +38,9 @@ class SpecificDetailAutoComplete(autocomplete.Select2QuerySetView):
         specof = self.forwarded.get('specof', None)
         if specof:
             qs = qs.filter(detail_field__id=specof)
+        else :
+            return Specific.objects.none()
+
         if self.q:
             qs = qs.filter(detail_value__istartswith=self.q)
 
@@ -22,6 +53,7 @@ class SpecificDetailAutoComplete(autocomplete.Select2QuerySetView):
             'detail_field_id':specific
         })
 
+@loginRequired
 
 class SpecificAutoComplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
@@ -32,13 +64,14 @@ class SpecificAutoComplete(autocomplete.Select2QuerySetView):
         if specific_of:
             cagetory = Cagetory.objects.get(id=specific_of)
             qs = cagetory.allspecific()
+        else :
+            return Specific.objects.none()
             
         if self.q:
             qs = qs.filter(specific_name__istartswith=self.q)
 
         return qs
 
-    
     def create_object(self, text):
         specific_of = self.forwarded.get('product_cagetory', None)
         return self.get_queryset().create(**{
