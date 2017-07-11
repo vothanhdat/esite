f82be0de78efd94eed1ad8ae0b46c4b13082eefe
Vo Thanh Dat <vothanhdat@outlook.com>
2017-07-11 17:33:58 +0700

    Fix some error

--- econ/admin.py
+++ econ/admin.py
@@ -6,14 +6,7 @@ from mptt.admin import TreeRelatedFieldListFilter,TreeNodeChoiceField
 
 from .models import Brand,Cagetory,Product,ProductImage,BaseUser,Agency,AgencyMember,AgencyPromotion,ProductPromotion,Specific,SpecificDetail,ProductSpecDetail
 from dal import autocomplete, forward
-
-from django.utils.translation import ugettext as _, ugettext_lazy
-from django.utils.encoding import smart_text
-from django.db.models.fields.related import ForeignObjectRel, ManyToManyField
-from mptt.compat import remote_field, remote_model
-from django.utils.translation import get_language_bidi
-from django.utils.html import format_html, mark_safe
-
+from .admin_customtreefilter import CustomTreeRelatedFieldListFilter
 
     
 
@@ -50,77 +43,6 @@ class SpecificDetailForm(forms.ModelForm):
         forward=['specof']
       ),
     }
-
-
-class CustomTreeRelatedFieldListFilter(TreeRelatedFieldListFilter):
-  template='custom_mptt_filter.html'
-  
-  class Media:
-    css = {
-      'all': (
-        'scss/custom_mptt_filter.scss',
-      )
-    }
-
-  def field_choices(self, field, request, model_admin):
-
-    return field.related_model._default_manager.filter(level__lte=0)
-
-  def choice_recuser(self,lookup_choices, cl,EMPTY_CHANGELIST_VALUE):
-    for model in lookup_choices:
-      yield {
-        'query_string': cl.get_query_string({
-          self.changed_lookup_kwarg: model.id,
-        }, [self.lookup_kwarg_isnull]),
-        'display': model,
-        'child': self.choice_recuser(
-          model.get_children().all(),
-          cl,
-          EMPTY_CHANGELIST_VALUE
-        ) if model.get_descendant_count() else None,
-      }
-
-
-  def choices(self, cl):
-    # #### MPTT ADDITION START
-    try:
-      # EMPTY_CHANGELIST_VALUE has been removed in django 1.9
-      from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
-    except:
-      EMPTY_CHANGELIST_VALUE = self.empty_value_display
-    # #### MPTT ADDITION END
-    yield {
-      'selected': self.lookup_val is None and not self.lookup_val_isnull,
-      'query_string': cl.get_query_string({
-        self.changed_lookup_kwarg: None,
-      }, [self.lookup_kwarg, self.lookup_kwarg_isnull]),
-      'display': _('All'),
-    }
-    for choice in self.choice_recuser(self.lookup_choices,cl,EMPTY_CHANGELIST_VALUE):
-      yield choice
-
-
-    # for model in self.lookup_choices:
-    #   print model
-    #   yield {
-    #     'query_string': cl.get_query_string({
-    #       self.changed_lookup_kwarg: model.id,
-    #     }, [self.lookup_kwarg_isnull]),
-    #     'display': model,
-    #     'child': model,
-    #   }
-    # if (isinstance(self.field, ForeignObjectRel) and
-    #     (self.field.field.null or isinstance(self.field.field, ManyToManyField)) or
-    #     remote_field(self.field) is not None and
-    #     (self.field.null or isinstance(self.field, ManyToManyField))):
-    #   yield {
-    #     'selected': bool(self.lookup_val_isnull),
-    #     'query_string': cl.get_query_string({
-    #       self.lookup_kwarg_isnull: 'True',
-    #     }, [self.lookup_kwarg]),
-    #     'display': EMPTY_CHANGELIST_VALUE,
-    #   }
-
 
 
 
---
+++ econ/admin_customtreefilter.py
@@ -0,0 +1,53 @@
+from mptt.admin import TreeRelatedFieldListFilter,TreeNodeChoiceField
+from django.utils.translation import ugettext as _, ugettext_lazy
+from django.utils.encoding import smart_text
+from django.db.models.fields.related import ForeignObjectRel, ManyToManyField
+from mptt.compat import remote_field, remote_model
+from django.utils.translation import get_language_bidi
+from django.utils.html import format_html, mark_safe
+
+
+    
+
+
+class CustomTreeRelatedFieldListFilter(TreeRelatedFieldListFilter):
+  template='custom_mptt_filter.html'
+  
+  def field_choices(self, field, request, model_admin):
+
+    return field.related_model._default_manager.filter(level__lte=0)
+
+  def choice_recuser(self,lookup_choices, cl,EMPTY_CHANGELIST_VALUE):
+    for model in lookup_choices:
+
+      yield {
+        'query_string': cl.get_query_string({
+          self.changed_lookup_kwarg: model.id,
+        }, [self.lookup_kwarg_isnull]),
+        'display': model,
+        'selected': (long(self.lookup_val) == model.id) if self.lookup_val else False,
+        'child': self.choice_recuser(
+          model.get_children().all(),
+          cl,
+          EMPTY_CHANGELIST_VALUE
+        ) if model.get_descendant_count() else None,
+      }
+
+
+  def choices(self, cl):
+    # #### MPTT ADDITION START
+    try:
+      # EMPTY_CHANGELIST_VALUE has been removed in django 1.9
+      from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
+    except:
+      EMPTY_CHANGELIST_VALUE = self.empty_value_display
+    # #### MPTT ADDITION END
+    yield {
+      'selected': self.lookup_val is None and not self.lookup_val_isnull,
+      'query_string': cl.get_query_string({
+        self.changed_lookup_kwarg: None,
+      }, [self.lookup_kwarg, self.lookup_kwarg_isnull]),
+      'display': _('All'),
+    }
+    for choice in self.choice_recuser(self.lookup_choices,cl,EMPTY_CHANGELIST_VALUE):
+      yield choice
